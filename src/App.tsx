559cdbd67fe7463a741604727f68f8d27e9eb183
Maksym Liashevskyi <maks.liashevskyi@gmail.com>
2024-11-10 20:25:01 +0100

    downloaded images and icons, created bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { FC, useEffect, useState } from "react";
+import { CSSProperties, FC, useEffect, useState } from "react";
 import { IBarometerData, IBasicData, IForecastDayData, IForecastHourData, IMoonPhaseData } from "./types/types";
 
 const App: FC = () => {
   const API_KEY = '0cedd8f2df2d480f8bd72437240911'
-  const [basicData, setBasicData] = useState<IBasicData | null>(null);
-  const [barometerData, setBarometerData] = useState<IBarometerData | null>(null);
-  const [moonPhaseData, setMoonPhaseData] = useState<IMoonPhaseData | null>(null);
-  const [forecastHourData, setForecastHourData] = useState<IForecastHourData | null>(null);
-  const [forecastDayData, setForecastDayData] = useState<IForecastDayData | null>(null);
-  const [city, setCity] = useState<string>("Torun")
+  const [basicData, setBasicData] = useState<IBasicData | null>(null)
+  const [barometerData, setBarometerData] = useState<IBarometerData | null>(null)
+  const [moonPhaseData, setMoonPhaseData] = useState<IMoonPhaseData | null>(null)
+  const [forecastHourData, setForecastHourData] = useState<IForecastHourData | null>(null)
+  const [forecastDayData, setForecastDayData] = useState<IForecastDayData | null>(null)
+  const [city, setCity] = useState<string>("Odessa")
+
+  const [bgI, setBgI] = useState<string | null>(null)
   
   useEffect(() => {
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=yes&alerts=no`)
-    .then(response => response.json())
-    .then(dataAPI => sortData(dataAPI))
+    try{
+      fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=yes&alerts=no`)
+      .then(response => response.json())
+      .then(dataAPI => sortData(dataAPI))
+    }
+    catch(e){
+      console.log(e)
+    }
   }, [])
 
-  
+  useEffect(() => {
+    setBgI(selectBackground)
+  })
+
   const sortData = (dataAPI: any): void => {
     setBasicData({
       country: dataAPI.location.country,
@@ -73,16 +83,129 @@ const App: FC = () => {
     })
   }
 
+  const selectBackground = ():string | null => {
+    console.log(basicData?.weather)
+    switch(basicData?.weather){
+      case 'Sunny':
+        return '../images/sunny.jpg'
+      case 'Clear':
+        return '../images/sunny.jpg'
+      case 'Partly Cloudy':
+        return '../images/cloudy.jpg'
+      case 'Partly cloudy':
+        return '../images/cloudy.jpg'
+      case 'Cloudy':
+        return '../images/cloudy.jpg'
+      case 'Overcast':
+        return '../images/cloudy.jpg'
+      case 'Mist':
+        return '../images/fog.jpg'
+      case 'Patchy rain possible':
+        return '../images/rainy.jpg'
+      case 'Patchy snow possible':
+        return '../images/snowy.jpg'
+      case 'Patchy sleet possible':
+        return '../images/rainy.jpg'
+      case 'Patchy freezing drizzle possible':
+        return '../images/rainy.jpg'
+      case 'Thundery outbreaks possible':
+        return '../images/thunderstorm.jpg'
+      case 'Blowing snow':
+        return '../images/snowy.jpg'
+      case 'Blizzard':
+        return '../images/snowy.jpg'
+      case 'Fog':
+        return '../images/fog.jpg'
+      case 'Freezing fog':
+        return '../images/fog.jpg'
+      case 'Patchy light drizzle':
+        return '../images/rainy.jpg'
+      case 'Light drizzle':
+        return '../images/rainy.jpg'
+      case 'Freezing drizzle':
+        return '../images/rainy.jpg'
+      case 'Heavy freezing drizzle':
+        return '../images/rainy.jpg'
+      case 'Patchy light rain':
+        return '../images/rainy.jpg'
+      case 'Light rain':
+        return '../images/rainy.jpg'
+      case 'Moderate rain at times':
+        return '../images/rainy.jpg'
+      case 'Moderate rain':
+        return '../images/rainy.jpg'
+      case 'Heavy rain at times':
+        return '../images/rainy.jpg'
+      case 'Heavy rain':
+        return '../images/rainy.jpg'
+      case 'Light freezing rain':
+        return '../images/rainy.jpg'
+      case 'Moderate or heavy freezing rain':
+        return '../images/rainy.jpg'
+      case 'Light sleet':
+        return '../images/snowy.jpg'
+      case 'Moderate or heavy sleet':
+        return '../images/snowy.jpg'
+      case 'Patchy light snow':
+        return '../images/snowy.jpg'
+      case 'Light snow':
+        return '../images/snowy.jpg'
+      case 'Patchy moderate snow':
+        return '../images/snowy.jpg'
+      case 'Moderate snow':
+        return '../images/snowy.jpg'
+      case 'Patchy heavy snow':
+        return '../images/snowy.jpg'
+      case 'Heavy snow':
+        return '../images/snowy.jpg'
+      case 'Ice pellets':
+        return '../images/rainy.jpg'
+      case 'Light rain shower':
+        return '../images/rainy.jpg'
+      case 'Moderate or heavy rain shower':
+        return '../images/rainy.jpg'
+      case 'Torrential rain shower':
+        return '../images/rainy.jpg'
+      case 'Light sleet showers':
+        return '../images/snowy.jpg'
+      case 'Moderate or heavy sleet showers':
+        return '../images/snowy.jpg'
+      case 'Light snow showers':
+        return '../images/snowy.jpg'
+      case 'Moderate or heavy snow showers':
+        return '../images/snowy.jpg'
+      case 'Light showers of ice pellets':
+        return '../images/snowy.jpg'
+      case 'Moderate or heavy showers of ice pellets':
+        return '../images/snowy.jpg'
+      case 'Patchy light rain with thunder':
+        return '../images/thunderstorm.jpg'
+      case 'Moderate or heavy rain with thunder':
+        return '../images/thunderstorm.jpg'
+      case 'Patchy light snow with thunder':
+        return '../images/thunderstorm.jpg'
+      case 'Moderate or heavy snow with thunder':
+        return '../images/thunderstorm.jpg'
+      default:
+        return null
+    }
+  }
+
+  const divStyle: CSSProperties = {
+    backgroundImage: `url(${bgI})`,
+    width: `${window.visualViewport?.width}px`,
+    height: `${window.visualViewport?.height}px`,
+    filter: 'brightness(80%)',
+    scale: 1,
+    backgroundRepeat: 'no-repeat'
+  }
+
   console.log(basicData)
-  console.log(barometerData)
-  console.log(moonPhaseData)
-  console.log(forecastHourData)
-  console.log(forecastDayData)
 
   return (
-    <>
-      
-    </>
+    <div style={divStyle}>
+
+    </div>
   )
 }
 
